edf01886e4d4da2eefbac059f2b2e3f35fa95b0d
grch <grch@DESKTOP-38G2HV1>
2024-05-01 14:32:51 +0800

    UI小改

--- ui.c
+++ ui.c
@@ -105,7 +105,7 @@ Ihandle* about_tab()
 	Ihandle* label2, *license;
 	
 	label1 = IupLabel("GitHub 仓库：");
-	link = IupLink("https://github.com/grch12", "https://github.com/grch12");
+	link = IupLink("https://github.com/grch12/splitgui", "https://github.com/grch12/splitgui");
 	
 	row = IupHbox(label1, link, NULL);
 	
@@ -115,7 +115,28 @@ Ihandle* about_tab()
 	IupSetAttribute(license, "MULTILINE", "YES");
 	IupSetAttribute(license, "EXPAND", "YES");
 	IupSetAttribute(license, "READONLY", "YES");
-	IupSetAttribute(license, "VALUE", "IUP (https://www.tecgraf.puc-rio.br/iup/)\n\n\
+	IupSetAttribute(license, "FONT", "Consolas, 10");
+	IupSetAttribute(license, "VALUE", "splitgui\n\n\
+MIT License\n\n\
+Copyright (c) 2024 grch12\n\n\
+Permission is hereby granted, free of charge, to any person obtaining a copy\n\
+of this software and associated documentation files (the \"Software\"), to deal\n\
+in the Software without restriction, including without limitation the rights\n\
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n\
+copies of the Software, and to permit persons to whom the Software is\n\
+furnished to do so, subject to the following conditions:\n\
+\n\
+The above copyright notice and this permission notice shall be included in all\n\
+copies or substantial portions of the Software.\n\
+\n\
+THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n\
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n\
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n\
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n\
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n\
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n\
+SOFTWARE.\n\n\
+IUP (https://www.tecgraf.puc-rio.br/iup)\n\n\
 Copyright (c) 1994-2023 Tecgraf/PUC-Rio.\n\
 Permission is hereby granted, free of charge, to any person obtaining a copy\n\
 of this software and associated documentation files (the \"Software\"), to deal\n\
